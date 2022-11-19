0d9cc0db4f6743078628f19bc581c46ab4c43040
Dennis Kühnberger <dennis.kuehnberger@gmx.de>
2022-11-19 16:24:51 +0100

    Rudimentäre Aufsetzung des Projekts
    Erstellung von Directories für BOs und Mapper, main.py und Administration erstellt, db file erstellt, Basis BO Klasse

---
+++ src/server/bo/BusinessObject.py
@@ -0,0 +1,18 @@
+from abc import ABC
+import datetime
+
+class BusinessObject(ABC):
+
+    def __init__(self):
+        self._id = 0
+        self._erstellungszeit = datetime.datetime.now()
+
+    def get_id(self):
+        return self._id
+
+    def set_id(self, value):
+        self._id = value
+
+    def get_erstellungszeit(self):
+        return self._erstellungszeit
+
