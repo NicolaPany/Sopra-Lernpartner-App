a389fd350a5156ed890a407aa2b04e7e3ad37d96
Dennis Kühnberger <dennis.kuehnberger@gmx.de>
2022-12-08 20:22:09 +0100

    Packages im Frontend für alles installiert
    Mit Hilfe des Bankbeispiels das Frontend etwas erweitert und Packages installiert

---
+++ frontend/my-react-app/src/firebaseconfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCuXr75MN2p_QnmyXTcd-pFZe9cXPYMcKk",
+  authDomain: "bankbeispielv2.firebaseapp.com",
+  projectId: "bankbeispielv2",
+  storageBucket: "bankbeispielv2.appspot.com",
+  messagingSenderId: "407802487159",
+  appId: "1:407802487159:web:37429114e3b72a8ba38172",
+  measurementId: "G-6CL8WD63C9"
+};
+
+export default firebaseConfig;
